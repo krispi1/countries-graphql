6e9d282a355fccc5606ac462787ab71049d84f3a
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-12 18:55:16 +0300

    Successfully fetch & render countries & their details on a table

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,15 +1,82 @@
 import React from 'react';
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks';
 
-function Countries() {
+const FETCH_COUNTRIES = gql`
+{
+  countries {
+    name,
+    code,
+    native,
+    phone,
+    currency,
+    languages {
+      name,
+      native
+    },
+    continent {
+      name
+    }
+  }
+}
+`; // FETCH_COUNTRIES
+
+
+function GetCountries() {
+  const { loading, error, data } = useQuery(FETCH_COUNTRIES);
+ 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {console.log(error)} </p>;
+
+  // Pull out countries array from the data object
+  const countries = data.countries;  
+  console.log(typeof countries);
+  console.log(`-------countries-------`);
+  console.log(countries[0]); // Inspect first country
+  console.log(`-------countries-------`);
+  
   return (
-    <div>
-      <h2>Countries</h2>
+    <div >
+      <table 
+        className="table table-hover table-striped table-dark tabl-row"
+      >
+        <thead className="table-head">
+          <tr>
+            <th>Country</th>
+            <th>Code</th>
+            <th>Continent</th>
+            <th>Language</th>
+            <th>Native</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+            
+        {
+          countries.map(country => (
+            //console.log(country.languages),
+            <tbody key={`${country.code}${country.name}`}>
+              <tr>
+                <td>{country.name}</td>
+                <td>{country.code}</td>
+                <td>{country.continent.name}</td>
+                <td>{country.languages.map(
+                  language => (`${language.name} `)
+                )}</td>
+                <td>{country.languages.map(
+                  language => (`${language.native} `)
+                )}</td>
+                <td>{country.currency}</td>
+              </tr>
+            </tbody>
+          ))
+        }     
+      </table>
     </div>
   );
+  
+} // end GetCountries
 
-} // end Countries()
-
-export default Countries;
+export default GetCountries;
 
 /** Countries Instructions
  * 
