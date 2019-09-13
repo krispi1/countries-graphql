b0ccece0fa84448fc6849a007f66ff3e6a26ba6b
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-13 18:05:10 +0300

    Add styling for loading spinner, display flag & phone code

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks';
+import spinner from '../assets/rotatingGlobe200w.webp';
 
 const FETCH_COUNTRIES = gql`
 {
@@ -21,10 +22,27 @@ const FETCH_COUNTRIES = gql`
 }
 `; // FETCH_COUNTRIES
 
+const loadingStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  backgroundColor: 'black',
+  margin: '0 auto',
+  marginTop: '30px',
+  color: 'cyan',
+  fontSize: '2em',
+  height: '50%',
+  width: '80%',
+}
 
 function GetCountries() {
   const { loading, error, data } = useQuery(FETCH_COUNTRIES);
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return <div style={loadingStyle}> 
+        <img src={spinner} alt="Loading Spinner" /> 
+        <br/><br/>
+        Loading....<br/>
+      </div>;
+  }
   if (error) return <p>Error: Error fetching data.{console.log(error)} </p>;
 
 // Pull out countries array from the data object
@@ -36,8 +54,8 @@ console.log(`-------countries-------`);
 
 let flagUrl = '';
 let flagBaseUrl = "https://www.countryflags.io/";
-let flagSize = "/flat/32.png"
-//let flagImage = `<img src=${flagUrl}>`;
+let flagStyle = "/flat"; // Either flat or shiny
+let flagSize = "/32.png"; // Sizes: 16, 24, 32, 64
 
   return (
     <div >
@@ -53,19 +71,25 @@ let flagSize = "/flat/32.png"
             <th>Language</th>
             <th>Native</th>
             <th>Currency</th>
+            <th>Phone</th>
           </tr>
         </thead>
             
         {
+          // Loop through all the countries
           countries.map(country => {
+
+            // Grab the country code of each country
             let flagCode = country.code;
-            flagUrl = `${flagBaseUrl}${flagCode}${flagSize}`;
+            // Generate custom url for each country's flag
+            flagUrl = `${flagBaseUrl}${flagCode}${flagStyle}${flagSize}`;
 
             return <tbody key={`${country.code}${country.name}`}>
                 <tr>
                   <td>{country.name}</td>
                   <td>{country.code}</td>
-                  <td><img src={flagUrl} alt={flagUrl}/></td>
+                  {/* Image alt name is important for accessibility purposes */}
+                  <td><img src={flagUrl} alt={`${country.name} flag`}/></td>
                   <td>{country.continent.name}</td>
                   <td>{country.languages.map(
                     language => (`${language.name} `)
@@ -74,6 +98,7 @@ let flagSize = "/flat/32.png"
                     language => (`${language.native} `)
                   )}</td>
                   <td>{country.currency}</td>
+                  <td>{country.phone}</td>
                 </tr>
               </tbody>
             }
