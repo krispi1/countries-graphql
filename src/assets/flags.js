e32f4444c6d54ddc3061caeb491d139f7730bb9c
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-17 16:27:31 +0300

    Create repository for flags' basic details

---
+++ src/assets/flags.js
@@ -0,0 +1,4 @@
+export const flagUrl = '';
+export const flagBaseUrl = "https://www.countryflags.io/";
+export const flagStyle = "/flat"; // Either flat or shiny
+export const flagSize = "/32.png"; // Sizes: 16, 24, 32, 64
