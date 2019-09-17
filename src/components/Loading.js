ba00f6697bb746421a866df8fa8ee4d58f33f9bd
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-17 12:39:46 +0300

    Create Loading.js to decouple loading spinner

---
+++ src/components/Loading.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import spinner from '../assets/rotatingGlobe200w.webp';
+
+export const loadingStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  backgroundColor: 'black',
+  margin: '0 auto',
+  marginTop: '30px',
+  color: 'cyan',
+  fontSize: '2em',
+  height: '50%',
+  width: '80%',
+}
+
+export default function Loading() {
+  return (
+    <div style={loadingStyle}>
+      <img src={spinner} alt="Loading Spinner" />
+      <br /><br />
+      Loading....<br />
+    </div>
+  )
+}
