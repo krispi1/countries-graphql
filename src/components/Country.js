cb8cb0512ba22f9cb90245d1aaf4fae8698ed53e
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-18 17:08:26 +0300

    Complete desired functionality for Country component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,74 +1,142 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+import { flagBaseUrl, flagStyle } from '../assets/flags';
+import Error from '../components/Error';
+import Loading from './Loading';
 
-const FETCH_COUNTRY = gql`
-{
-  country(code: "KE"){
-    code,
-    name,
-    native,
-    phone,
-    currency,
-    emoji,
-    emojiU,
-    continent {
-      code,
-      name, 
-      countries {
+function GetCountry({ match }) {
+
+  let code = match.params.code.toUpperCase();
+  //console.log(code); // For debugging
+
+  // Destructure the useQuery fetch results
+  const { loading, error, data } = useQuery(
+    gql`
+      query {
+        country(code: "${code}") {
+        code,
         name,
-        code
+        native,
+        phone,
+        currency,
+        emoji,
+        emojiU,
+        continent {
+          code,
+          name, 
+          countries {
+            name,
+            code
+          }
+        },
+        languages {
+          code,
+          name,
+          native
+        }
       }
-    },
-    languages {
-      code,
-      name,
-      native
     }
-  }
-}
-`; // end FETCH_COUNTRY query
+    `
+  );
+  
+  /* 
+  // For debugging
+  
+  console.log(`Country Code: ${code}`)
+  console.log(match);
+  console.log(data); 
+  */
 
-function GetCountry() {
+  if (loading) return <Loading />;
 
-  // Destructure the useQuery function results
-  const { loading, error, data } = useQuery(FETCH_COUNTRY);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: Error fetching data. Please check 
-    your internet and try again. {console.log(error)} </p>;
+  if (error) return <Error />;
   
   // Pull out country from data object
   const country = data.country;
   
-  console.log(typeof country); // For debugging
+  // console.log(typeof country); // For debugging
   console.log(country); // For debugging
+  
+  // Grab the country code of current country & set up flag
+  let flagCode = code;
+  let flagShiny = "/shiny";
+  let flagSize = "/64.png";
+  let flagUrl = `${flagBaseUrl}${flagCode}${flagStyle}${flagSize}`;  
+  let shinyFlag = `${flagBaseUrl}${flagCode}${flagShiny}${flagSize}`;
+  //console.log(flagUrl); // For debugging
 
   return (
-    <div>
-      {
-        /*Example country
-        code: null
-        continent: {code: "AF", name: "Africa", countries: Array(58), __typename: "Continent"}
-        currency: "KES"
-        emoji: "🇰🇪"
-        emojiU: "U+1F1F0 U+1F1EA"
-        languages: (2) [{…}, {…}]
-        name: "Kenya"
-        native: "Kenya"
-        phone: "254" */
-      }
+    <Fragment>
+      <div className="content-area">
+        <h2><strong>{`${country.name.toUpperCase()}`}</strong></h2>
+        <div><img 
+          src={flagUrl} 
+          alt={`${country.name} flag`}/>
+        </div>
+        <div>
+          <table className="table">
+            <thead className="thead-dark">
+              <tr>
+                <th>Item</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Currency</td>
+                <th scope="row">{country.currency}</th>
+              </tr>
+              <tr>
+                <td>Phone</td>
+                <th scope="row">{country.phone}</th>
+              </tr>
+              <tr>
+                <td>Native</td>
+                <th scope="row">{country.native}</th>
+              </tr>
+              <tr>
+                <td>Emoji</td>
+                <th scope="row">{country.emoji}</th>
+              </tr>
+              <tr>
+                <td>EmojiU</td>
+                <th scope="row">{country.emojiU}</th>
+              </tr>
+            </tbody>
+          </table>
+          <div className="shiny-flag">
+            <img 
+              src={shinyFlag} 
+              alt={`${country.name} flag`}/>
+          </div>
+        </div>
+      </div>
+    </Fragment>
 
-      <h3>{`${country.name} ${country.phone}`}</h3>
-      <h4>{`${country.currency} ${country.native}`}</h4>
-      <h5>{`${country.emoji} ${country.emojiU}`}</h5>
-    </div>
-
-  ) // end return()
+  ) // end return
   
 } // end GetCountry()
 
 export default GetCountry;
 
-// `/countries/(:code)` must render the currency and the area
-// code (phone) of that country.
+/** Country Instructions
+ * 
+ * `/countries/(:code)` must render the currency and the area
+ * code (phone) of that country.
+ * 
+ */
+
+/**Example country
+
+  code: null
+  continent: {code: "AF", name: "Africa", countries: Array(58), __typename: "Continent"}
+  currency: "KES"
+  emoji: "🇰🇪"
+  emojiU: "U+1F1F0 U+1F1EA"
+  languages: (2) [{…}, {…}]
+  name: "Kenya"
+  native: "Kenya"
+  phone: "254" 
+  
+  */
