a87e2eb7fb404904d84d648aa0855e09f9e8d2e1
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-19 19:20:10 +0300

    Make slight style changes

--- src/App.js
+++ src/App.js
@@ -6,73 +6,72 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
+import Home from './components/Home';
 import GetCountries from './components/Countries';
-//import GetCountry from './components/Country';
+import GetCountry from './components/Country';
 
 const client = new ApolloClient({
   uri: 'https://countries.trevorblades.com/'
 });
 
-/* // Trial data fetch -- turns out successful
-client
+// Trial data fetch
+/* client
   .query({
     query: gql`
-    {
-      country(code: "KE"){
-        code,
-        name,
-        native,
-        phone,
-        currency,
-        emoji,
-        emojiU,
-        continent {
-          code,
-          name, 
-          countries {
-            name,
-            code
-          }
-        },
-        languages {
-          code,
+      query {
+        countries {
           name,
-          native
+          code,
+          native,
+          phone,
+          currency,
+          languages {
+            name,
+            native
+          },
+          continent {
+            name
+          }
         }
       }
-    }
     `
-}).then(result => console.log("result")) */
+  })
+  .then(result => {
+    console.log(`result: ${result}`);
+    let countries = result.data.countries;
+    console.log(countries);
+  }) */
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      <Nav />
+        <Nav />
         <div className="container">
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/countries" exact component={GetCountries} />
-            {/* <Route path="/countries/:id" component={GetCountry} /> */}
-          {<GetCountries />}
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/countries" component={GetCountries} />
+            <Route exact path="/countries/:code" component={GetCountry} />
+            <Route component={Page404} />
           </Switch>
-          {/* GetCountry /> */}
         </div>
       </Router>
     </ApolloProvider>
-  );
+    
+  ); // end return
 
 } // end App()
 
-function Home() {
-  return (
-    <div className="container">
-      <h1>Scoutbase Frontend Siele</h1>  
-    </div>
-  );
-
-} // end Home()
+function Page404({ location }) {
+  const style404 = {
+    marginTop: '30px',
+    fontSize: '2.9em'
+  }
+  return <div className="content-area">
+    <h2 style={style404}>Sorry, we couldn't find the page you requested:<br /> <code>{location.pathname}</code></h2>
+  </div>
+}
 
 export default App;
 
@@ -85,21 +84,21 @@ This task is for demonstrating your understanding of HTML, CSS, Javascript, Reac
 If you’re doing the front-end only, you must use the https://countries.trevorblades.com endpoint for GraphQL API.
 
 Preferred libraries:
-  1. `styled-components` for styling
-  2. `apollo-client` for consuming GraphQL API
-  3. `react-router` or any alternative to implement routing
+  1. `styled-components` for styling ------used Bootstrap
+  2. `apollo-client` for consuming GraphQL API ------completed
+  3. `react-router` or any alternative to implement routing ------completed
 
 Instructions:
 
 1. Create a `create-react-app` repo. ------completed
 2. Add a router with routes:
-  - `/` - for displaying basic links for the other routes ------incomplete
+  - `/` - for displaying basic links for the other routes ------completed
   - `/countries` - for requesting GraphQL API and rendering the list ------completed
-  - `/countries/(:code)` - for requesting GraphQL API and rendering the properties of a continent ------incomplete
+  - `/countries/(:code)` - for requesting GraphQL API and rendering the properties of a continent ------completed
 3. Design is totally by you.
 4. Countries list at `/countries` must contain the list of countries and the languages spoken in that country. ------completed
    Both in English and native languages. It should also contain the continent it is located in. ------completed
-5. `/countries/(:code)` must render the currency and a area code (phone) of that country. ------incomplete
-6. Routes must be fully loadable with a direct link. `/countries/CI` must render the page for Ivory Coast, for example. ------incomplete
-7. End. 
+5. `/countries/(:code)` must render the currency and a area code (phone) of that country. ------completed
+6. Routes must be fully loadable with a direct link. `/countries/CI` must render the page for Ivory Coast, for example. ------completed
+7. End.
 */
