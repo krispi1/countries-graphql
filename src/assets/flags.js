9834e745fbd225edde96a7f3b071bb7d8c4a6ec8
Geoffrey Siele <sielekrisp@gmail.com>
2019-09-20 09:03:24 +0300

    Set app title (Index.html) and delete unnecessary files

--- src/assets/flags.js
+++
@@ -1,4 +0,0 @@
-export const flagUrl = '';
-export const flagBaseUrl = "https://www.countryflags.io/";
-export const flagStyle = "/flat"; // Either flat or shiny
-export const flagSize = "/32.png"; // Sizes: 16, 24, 32, 64
